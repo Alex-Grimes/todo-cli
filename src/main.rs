7ddac536b786d8b8be83311dd18ebfba33e53336
alex <alexgrimes.se@gmail.com>
2023-02-20 13:15:53 -0500

    add a save function

--- src/main.rs
+++ src/main.rs
@@ -15,4 +15,13 @@ impl Todo {
     fn insert(&mut self, key: String) {
         self.map.insert(key, true);
     }
+
+    fn save(self) -> Result<(), std::io::Error> {
+        let mut content = String::new();
+        for (k, v) in self.map {
+            let record = format!("{}\t{}\n", k, v);
+            content.push_str(&record)
+        }
+        std::fs::write("db.txt", content)
+    }
 }
