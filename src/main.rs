efe3a4f118322d9a7268467e86fb1ba288798a8b
alex <alexgrimes.se@gmail.com>
2023-06-25 13:17:11 -0400

    Todo CLI Tool Completed

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,10 @@ fn main() {
     } else if action == "complete" {
         match todo.complete(&item) {
             None => println!("'{}' is not present in the list", item),
-            Some(_) => {
-                match todo.save() {
-                    Ok(_) => println!("Saved!"),
-                    Err(err) => println!("Error: {}", err),
-                },
-            }
+            Some(_) => match todo.save() {
+                Ok(_) => println!("Saved!"),
+                Err(err) => println!("Error: {}", err),
+            },
         }
     }
 }
@@ -30,16 +28,39 @@ struct Todo {
 }
 
 impl Todo {
+    fn new() -> Result<Todo, std::io::Error> {
+        let f = std::fs::OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open("db.json")?;
+
+        match serde_json::from_reader(f) {
+            Ok(map) => Ok(Todo { map }),
+            Err(err) if err.is_eof() => Ok(Todo {
+                map: HashMap::new(),
+            }),
+            Err(err) => panic!("An error occurred: {}", err),
+        }
+    }
+
     fn insert(&mut self, key: String) {
         self.map.insert(key, true);
     }
 
-    fn save(self) -> Result<(), std::io::Error> {
-        let mut content = String::new();
-        for (k, v) in self.map {
-            let record = format!("{}\t{}\n", k, v);
-            content.push_str(&record)
+    fn save(self) -> Result<(), Box<dyn std::error::Error>> {
+        let f = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open("db.json")?;
+        serde_json::to_writer_pretty(f, &self.map)?;
+        Ok(())
+    }
+
+    fn complete(&mut self, key: &String) -> Option<()> {
+        match self.map.get_mut(key) {
+            Some(v) => Some(*v = false),
+            None => None,
         }
-        std::fs::write("db.txt", content)
     }
 }
