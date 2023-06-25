4a4b5d5d98ddfe306d682c143f03e25626587fbb
alex <alexgrimes.se@gmail.com>
2023-06-25 12:41:39 -0400

    adding complete  option

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,25 @@ fn main() {
     let action = std::env::args().nth(1).expect("Please specify an action");
     let item = std::env::args().nth(2).expect("Please specify an item");
 
-    let mut todo = Todo {
-        map: HashMap::new(),
-    };
+    let mut todo = Todo::new().expect("Initialisation of db failed");
+
     if action == "add" {
         todo.insert(item);
         match todo.save() {
             Ok(_) => println!("Saved!"),
             Err(err) => println!("Error: {}", err),
         }
+    } else if action == "complete" {
+        match todo.complete(&item) {
+            None => println!("'{}' is not present in the list", item),
+            Some(_) => {
+                match todo.save() {
+                    Ok(_) => println!("Saved!"),
+                    Err(err) => println!("Error: {}", err),
+                },
+            }
+        }
     }
-    println!("{:?}, {:?}", action, item);
 }
 
 struct Todo {
